7d94b93855254c9f8c3daac1c17b5ba60e207cd8
Ani Gevorgyan <anigevorgyann@gmail.com>
2024-04-04 13:35:15 +0400

    removed vscode

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+let count = 0;
+
+
+
+
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+<template>
+ 
 
   <main>
-    <TheWelcome />
+<h3> 
+
+{{count}}
+</h3>
+    
   </main>
 </template>
 
